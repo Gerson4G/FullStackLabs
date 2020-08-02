f62ad28e15a7539b7eb1c548bf350484d5043207
Gerson Goatache <gersongoatache@gmail.com>
2020-08-02 01:23:15 -0400

    block styling and showing

--- src/components/Block.jsx
+++ src/components/Block.jsx
@@ -1,5 +1,22 @@
 import React from 'react';
+import '../styles/block.css';
 
-const Block = ({data: {id}}) => <div>Im block {id}</div>
+const formatId = (id) => {
+    let format = id;
+    if(id.length === 1){
+        format = `00${id}`
+    }
+    else if(id.length === 2){
+        format = `0${id}`
+    }
+    return format
+}
+
+const Block = ({data: {id, attributes: {data: blockData}}}) => (
+    <div className="block-container">
+        <div className="block-id">{formatId(id)}</div>
+        <div className="block-data">{blockData}</div>
+    </div>
+)
 
 export default Block;
